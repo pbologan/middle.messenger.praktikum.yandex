3fbe54b5d1858f65db716df6617beb6541f30776
Pavel Bologan <p.bologan@gmail.com>
2022-05-27 16:22:36 +0300

    Rename index.js to server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+`use strict`
+
+const express = require(`express`);
+const { resolve } = require(`path`);
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(resolve(__dirname, `dist`)));
+
+app.get(`/`, (req, res) => {
+    res.sendFile(resolve(__dirname, `dist`, `index.html`));
+});
+
+app.listen(PORT, () => console.info(`Сервер запущен на порту ${PORT}`));
