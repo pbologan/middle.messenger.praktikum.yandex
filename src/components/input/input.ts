635cfcc33f53249409cf9979212d7e671a0edc7a
Pavel Bologan <p.bologan@gmail.com>
2022-08-14 20:44:44 +0400

    Add Input component

---
+++ src/components/input/input.ts
@@ -0,0 +1,36 @@
+import './input.css';
+import { Block } from '../../core';
+
+export interface InputProps {
+  id?: string;
+  type?: 'text' | 'password' | 'email';
+  placeholder?: string;
+  onFocus?: (e: FocusEvent) => void;
+  onBlur?: (e: FocusEvent) => void;
+  onInput?: (e: InputEvent) => void;
+  ref?: Block;
+}
+
+export default class Input extends Block {
+  public static componentName = 'Input';
+
+  constructor({
+    onFocus, onBlur, onInput, ...props
+  } : InputProps) {
+    super({
+      ...props,
+      events: {
+        input: onInput,
+        blur: onBlur,
+        focus: onFocus,
+      },
+    });
+  }
+
+  // language=hbs
+  // eslint-disable-next-line class-methods-use-this
+  render(): string {
+    // eslint-disable-next-line @typescript-eslint/quotes
+    return `<input class="input" type="{{type}}" placeholder="{{placeholder}}" name="{{id}}" id="{{id}}">`;
+  }
+}
