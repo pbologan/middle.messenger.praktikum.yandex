927c9180773643cdf752c5759a2f9871a06a94ed
Pavel Bologan <p.bologan@gmail.com>
2022-08-15 16:31:12 +0400

    Make data and method options optional in http-transport.ts

--- src/utils/http-transport.ts
+++ src/utils/http-transport.ts
@@ -10,22 +10,20 @@ interface DataObject {
 }
 
 interface Options {
-  data: Data;
-  method: METHOD,
-  headers?: Pair<string, string>[],
+  data?: DataObject;
+  method?: Method,
+  headers?: DataObject,
   retries?: number,
   timeout?: number;
 }
 
-function queryStringify(data: Data) {
-  const dataItems = data.items;
-
-  if (dataItems.length === 0) {
+function queryStringify(data?: DataObject) {
+  if (!data || Object.keys(data).length === 0) {
     return '';
   }
 
-  return `?${dataItems
-    .map((item: Pair<string, string>) => `${item.key}=${item.value}`)
+  return `?${Object.keys(data)
+    .map((key: string) => `${key}=${data[key]}`)
     .join('&')}`;
 }
 
@@ -33,64 +31,63 @@ export default class HTTPTransport {
   get(url: string, options: Options) {
     return this.request(
       url + queryStringify(options.data),
-      { ...options, method: METHOD.GET },
+      { ...options, method: Method.GET },
       options.timeout,
     );
   }
 
   put(url: string, options: Options) {
-    return this.request(url, { ...options, method: METHOD.PUT }, options.timeout);
+    return this.request(url, { ...options, method: Method.PUT }, options.timeout);
   }
 
   post(url: string, options: Options) {
-    return this.request(url, { ...options, method: METHOD.POST }, options.timeout);
+    return this.request(url, { ...options, method: Method.POST }, options.timeout);
   }
 
   delete(url: string, options: Options) {
-    return this.request(url, { ...options, method: METHOD.DELETE }, options.timeout);
+    return this.request(url, { ...options, method: Method.DELETE }, options.timeout);
   }
 
-  // eslint-disable-next-line class-methods-use-this
-  private sendData(xhr: XMLHttpRequest, data: Data, method: METHOD) {
-    if (!data || method === METHOD.GET) {
+  private sendData(xhr: XMLHttpRequest, method: Method, data?: DataObject) {
+    if (!data || method === Method.GET) {
       xhr.send();
     } else {
-      const dataForSend = {} as any;
-      data.items.forEach((item: Pair<string, string>) => {
-        dataForSend[item.key] = item.value;
-      });
-      xhr.send(JSON.stringify(dataForSend));
+      xhr.send(JSON.stringify(data));
     }
   }
 
-  request(url: string, options: Options, timeout = 5000) {
+  private request(url: string, options: Options, timeout = 5000) {
     return new Promise((resolve, reject) => {
       const {
-        headers = [], retries = 0, method, data,
+        headers, retries = 0, method, data,
       } = options;
 
       let retriesCount = retries;
 
       if (!method) {
-        // eslint-disable-next-line prefer-promise-reject-errors
         reject('No method provided');
+        return;
       }
 
       const xhr = new XMLHttpRequest();
       xhr.open(method, url);
       xhr.timeout = timeout;
 
-      headers.forEach((item: Pair<string, string>) => {
-        xhr.setRequestHeader(item.key, item.value);
-      });
+      if (headers) {
+        Object.keys(headers).forEach((key: string) => {
+          const header = headers[key];
+          if (header) {
+            xhr.setRequestHeader(key, header);
+          }
+        });
+      }
 
       xhr.onload = () => resolve(xhr);
       xhr.onerror = (event) => {
         if (retriesCount > 0) {
           setTimeout(() => {
             xhr.open(method, url);
-            this.sendData(xhr, data, method);
-            // eslint-disable-next-line no-plusplus
+            this.sendData(xhr, method, data);
             retriesCount--;
           }, 100);
         } else {
@@ -101,7 +98,7 @@ export default class HTTPTransport {
       xhr.ontimeout = (event) => reject(event);
       xhr.onabort = (event) => reject(event);
 
-      this.sendData(xhr, data, method);
+      this.sendData(xhr, method, data);
     });
   }
 }
