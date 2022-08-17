656e6dc3463ebd50c0b32301353a633d18e219dc
Pavel Bologan <p.bologan@gmail.com>
2022-08-17 15:05:09 +0400

    Add MessagesList component

---
+++ src/pages/chat/messages-list/messages-list.ts
@@ -0,0 +1,16 @@
+import './messages-list.css';
+import { Block } from '../../../core';
+
+interface MessagesListProps { }
+
+export default class MessagesList extends Block<MessagesListProps> {
+  public static override componentName = 'MessagesList';
+
+  override render(): string {
+    // language=hbs
+    return `
+      <div class="messages__messages-list-layout">
+      </div>
+    `;
+  }
+}
