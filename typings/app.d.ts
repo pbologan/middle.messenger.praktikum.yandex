029c5492a05762fd322105d14311ee7a9efba155
Pavel Bologan <p.bologan@gmail.com>
2023-02-04 22:01:03 +0400

    Add type aliases for APIError, AppState, User, UserDTO

--- typings/app.d.ts
+++ typings/app.d.ts
@@ -3,6 +3,44 @@ declare global {
 
   export type Keys<T extends Record<string, unknown>> = keyof T;
   export type Values<T extends Record<string, unknown>> = T[Keys<T>];
+
+  export type APIError = {
+    reason: string;
+  };
+
+  export type AppState = {
+    appIsReady: boolean;
+    page: Page | null;
+    isLoading: boolean;
+    loginFormError: string | null;
+    user: User | null;
+  };
+
+  export type Page = {
+
+  };
+
+  export type UserDTO = {
+    id: number;
+    login: string;
+    first_name: string;
+    second_name: string;
+    display_name: string;
+    avatar: string;
+    phone: string;
+    email: string;
+  };
+
+  export type User = {
+    id: number;
+    login: string;
+    firstName: string;
+    secondName: string;
+    displayName: string;
+    avatar: string;
+    phone: string;
+    email: string;
+  };
 }
 
 export {};
