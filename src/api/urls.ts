67b5483e24fbecd12ed99d3f458cd0de98b683a0
Pavel Bologan <p.bologan@gmail.com>
2023-02-11 21:02:09 +0400

    Add users urls

--- src/api/urls.ts
+++ src/api/urls.ts
@@ -10,12 +10,15 @@ export const LOGOUT = `/auth/logout`;
 // Chats
 
 export const CHATS = `/chats`;
-export const GET_CHAT_SENT_FILES = (id: string) => `${CHATS}/${id}/files`;
-export const ARCHIVE_CHATS = `${CHATS}/archive`;
-export const UNARCHIVE_CHAT = `${CHATS}/unarchive`;
-export const GET_COMMON_CHAT_WITH_CURRENT_CHAT_USER = (id: string) => `${CHATS}/${id}/common`;
-export const GET_CHAT_USERS = (id: string) => `${CHATS}/${id}/users`;
-export const GET_NEW_MESSAGES_COUNT = (id: string) => `${CHATS}/new/${id}`;
+export const getChatUsersUrl = (id: number) => `${CHATS}/${id}/users`;
+export const getNewMessagesCountUrl = (id: number) => `${CHATS}/new/${id}`;
 export const UPLOAD_CHAT_AVATAR = `${CHATS}/avatar`;
 export const CHATS_USERS = `${CHATS}/users`;
-export const CHATS_TOKEN = (id: string) => `${CHATS}/token/${id}`;
+
+// Users
+
+export const USER = '/user';
+export const USER_PROFILE = `${USER}/profile`;
+export const USER_PROFILE_AVATAR = `${USER}${USER_PROFILE}/avatar`;
+export const USER_PASSWORD = `${USER}/password`;
+export const USER_SEARCH = `${USER}/search`;
