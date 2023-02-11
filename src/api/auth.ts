c1ef7a83f5a6af3327aeff66121fcfb82f828f7f
Pavel Bologan <p.bologan@gmail.com>
2023-02-11 18:43:32 +0400

    Add AuthApi

---
+++ src/api/auth.ts
@@ -0,0 +1,40 @@
+import { HTTPTransport } from '../utils/http-transport';
+import {
+  BASE_URL, GET_USER_INFO, LOGOUT, SIGN_IN, SIGN_UP,
+} from './urls';
+import { APIError } from '../models/error';
+import { SignInRequest, SignUpRequest, SignUpResponse } from '../models/api-types';
+import { UserDTO } from '../models/user';
+
+export class AuthApi {
+  private static instance: AuthApi | null = null;
+
+  private readonly httpClient: HTTPTransport;
+
+  private constructor() {
+    this.httpClient = new HTTPTransport(BASE_URL);
+  }
+
+  public static getInstance(): AuthApi {
+    if (!AuthApi.instance) {
+      AuthApi.instance = new AuthApi();
+    }
+    return AuthApi.instance;
+  }
+
+  public signUp(userData: SignUpRequest) {
+    return this.httpClient.post<SignUpResponse | APIError>(SIGN_UP, { data: userData });
+  }
+
+  public signIn(userData: SignInRequest) {
+    return this.httpClient.post<APIError>(SIGN_IN, { data: userData });
+  }
+
+  public logout() {
+    return this.httpClient.post(LOGOUT);
+  }
+
+  public getUserInfo() {
+    return this.httpClient.get<UserDTO | APIError>(GET_USER_INFO);
+  }
+}
