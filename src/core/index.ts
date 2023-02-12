ce572193d9bba213eface625a336209589fa509e
Pavel Bologan <p.bologan@gmail.com>
2023-02-13 00:09:18 +0400

    Add exports Store, registerComponent, BrowserRouter to index.ts

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,3 +1,6 @@
 export { EventBus } from './EventBus';
 export { Block, BlockClass } from './Block';
 export { renderDOM } from './renderDOM';
+export { Store } from './Store';
+export { registerComponent } from './registerComponent';
+export { BrowserRouter } from './router';
