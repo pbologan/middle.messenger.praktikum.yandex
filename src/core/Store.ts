b0f904c41d1f741fad248c3114db3b86fd92596d
Pavel Bologan <p.bologan@gmail.com>
2023-02-12 16:28:14 +0400

    Make Store singleton

--- src/core/Store.ts
+++ src/core/Store.ts
@@ -1,26 +1,41 @@
 import { EventBus } from './EventBus';
-import { Action } from '../models/app';
+import { Action, AppState } from '../models/app';
+import { initialState } from '../utils/initialState';
 
-export class Store<State extends Record<string, any>> extends EventBus {
-  private state: State = {} as State;
+export class Store extends EventBus {
+  public static readonly STATE_CHANGED = 'state changed';
 
-  constructor(state: State) {
+  private static instance: Store | null = null;
+
+  private state: AppState;
+
+  private constructor(state: AppState) {
     super();
     this.state = state;
     this.setState(state);
+  }
+
+  public static getInstance() {
+    if (!Store.instance) {
+      Store.instance = new Store(initialState);
+    }
+    return Store.instance;
   }
 
   public getState() {
     return this.state;
   }
 
-  public setState(nextState: Partial<State>) {
+  public setState(nextState: Partial<AppState>) {
     const prevState = { ...this.state };
     this.state = { ...prevState, ...nextState };
-    this.emit('changed', prevState, nextState);
+    this.emit(Store.STATE_CHANGED, prevState, nextState);
   }
 
-  public dispatch(nextStateOrAction: Partial<State> | Action<State>, payload?: any) {
+  public dispatch(
+    nextStateOrAction: Partial<AppState> | Action<AppState>,
+    payload?: any,
+  ) {
     if (typeof nextStateOrAction === 'function') {
       nextStateOrAction(this.dispatch.bind(this), this.state, payload);
     } else {
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,14 @@
 import './styles.css';
 import { registerComponents } from './utils/registerComponents';
 import { BrowserRouter } from './core/router';
-import { initialState } from './utils/initialState';
 import { initRouter } from './utils/initRouter';
 import { Store } from './core/Store';
-import { AppState } from './models/app';
-
-declare global {
-  interface Window {
-    store: Store<AppState>,
-    router: BrowserRouter,
-  }
-}
 
 registerComponents();
 
 document.addEventListener('DOMContentLoaded', () => {
-  const store = new Store<AppState>(initialState);
+  const store = Store.getInstance();
   const router = BrowserRouter.getInstance();
-
-  window.store = store;
-  window.router = router;
 
   initRouter(router, store);
 });
