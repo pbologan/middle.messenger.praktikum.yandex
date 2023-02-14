9f917271d55a5a3a000d22da5e57ca048277c895
Pavel Bologan <p.bologan@gmail.com>
2023-02-14 15:06:27 +0400

    Add initApp service

---
+++ src/service/initApp.ts
@@ -0,0 +1,21 @@
+import { AppState, Dispatch, Page } from '../models/app';
+import { localStorageUtils } from '../utils/localStorageUtils';
+import { BrowserRouter } from '../core';
+import { AuthApi } from '../api';
+import { apiHasError } from '../utils/apiHasError';
+import { transformUserDTO } from '../models/user';
+
+export async function initApp(dispatch: Dispatch<AppState>) {
+  const storedPage = localStorageUtils.getCurrentPage();
+  const router = BrowserRouter.getInstance();
+  const response = await AuthApi.getInstance().getUserInfo();
+
+  if (apiHasError(response)) {
+    router.go(storedPage || Page.LOGIN);
+    return;
+  }
+
+  dispatch({ user: transformUserDTO(response) });
+
+  router.go(storedPage || Page.CHAT);
+}
