55497b94c213957ed2146830ae197562d1fd3f14
Pavel Bologan <p.bologan@gmail.com>
2023-02-17 14:16:43 +0400

    Add type, onChange, accept props to the Input component

--- src/components/input/input.ts
+++ src/components/input/input.ts
@@ -5,24 +5,27 @@ export interface InputProps {
   value?: string;
   className: string;
   id?: string;
-  type?: 'text' | 'password' | 'email';
+  type?: 'text' | 'password' | 'email' | 'file';
   placeholder?: string;
   onFocus?: (e: FocusEvent) => void;
   onBlur?: (e: FocusEvent) => void;
+  onChange?: (e?: Event) => void;
   onInput?: (e: InputEvent) => void;
   ref?: Block<object>;
+  accept?: string,
   events: {
     input?: ((e: InputEvent) => void) | undefined,
     blur?: ((e: FocusEvent) => void) | undefined,
     focus?: ((e: FocusEvent) => void) | undefined,
+    change?: ((e?: Event) => void) | undefined,
   }
 }
 
-export default class Input extends Block<InputProps> {
+export class Input extends Block<InputProps> {
   public static override componentName = 'Input';
 
   constructor({
-    onFocus, onBlur, onInput, ...props
+    onFocus, onBlur, onInput, onChange, ...props
   } : InputProps) {
     super({
       ...props,
@@ -30,12 +33,20 @@ export default class Input extends Block<InputProps> {
         input: onInput,
         blur: onBlur,
         focus: onFocus,
+        change: onChange,
       },
     });
   }
 
   // language=hbs
   override render(): string {
-    return `<input class="{{className}}" type="{{type}}" placeholder="{{placeholder}}" name="{{id}}" id="{{id}}" value="{{value}}">`;
+    return `<input 
+              class="{{className}}"
+              type="{{type}}"
+              placeholder="{{placeholder}}"
+              name="{{id}}" id="{{id}}"
+              value="{{value}}"
+              onchange="{{onChange}}"
+              accept="{{accept}}">`;
   }
 }
